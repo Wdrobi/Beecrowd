152ad2eb92af4087fbedb4f130ed5e5ebec2f826
wdrobi <md.robiulislam.qcsc@gmail.com>
2023-01-29 15:05:06 +0600

    New Code

---
+++ Beginner/2791.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int C1, C2, C3, C4;
+    cin >> C1 >> C2 >> C3 >> C4;
+    if (C1 == 1)
+    {
+        cout << 1 << endl;
+    }
+    else if (C2 == 1)
+    {
+        cout << 2 << endl;
+    }
+    else if (C3 == 1)
+    {
+        cout << 3 << endl;
+    }
+    else if (C4 == 1)
+    {
+        cout << 4 << endl;
+    }
+    return 0;
+}
